a25614556fd5ad77454e46b124d4f66619fcbaf2
kasper Stecher <stecherkasper@gmail.com>
2026-02-19 15:29:06 +0100

    added a camerafollow script

--- Parallax/Assets/💻 Scripts/Player/CameraController.cs
+++ Parallax/Assets/💻 Scripts/Player/CameraController.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Transform FollowTarget, LookTarget;
+    public float FollowSpeed = 10f;
+
+
+public void LateUpdate()
     {
-        
+        Vector3 targetPosition = FollowTarget.position;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, FollowSpeed * Time.deltaTime);
+
+        transform.LookAt(LookTarget);
     }
 }
