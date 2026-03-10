c886410d7ffcd0e48a9179ccd90609f0723373df
Kasper Bruun Bentsen <202103249@post.au.dk>
2026-03-10 09:29:37 +0100

    Added multiplayer UI

---
+++ Parallax/Assets/💻 Scripts/Multiplayer/LobbyCodeDisplay.cs
@@ -0,0 +1,22 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class LobbyCodeDisplay : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (Multiplayer.Instance == null) return;
+        
+        float w = 220f, h = 40f;
+        float x = 10f, y = 10f;
+        float spacing = 10f;
+
+        string code = Multiplayer.Instance.CurrentLobbyCode;
+
+        if (!string.IsNullOrEmpty(code))
+        {
+            GUI.Label(new Rect(x, y, 300, h), "Lobby Code: " + code);
+            if (GUI.Button(new Rect(x, y + (h + 10), w, h), "Leave lobby")) Multiplayer.Instance.Disconnect(); 
+        }
+    }
+}
