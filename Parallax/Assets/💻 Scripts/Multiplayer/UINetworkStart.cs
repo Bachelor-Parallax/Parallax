b832b353f9c644eb12afce9f38b8cf7ae5935d05
amarstang <35458474+amarstang@users.noreply.github.com>
2026-03-31 09:58:26 +0200

    Lobby party UI intergration - not done

---
+++ Parallax/Assets/💻 Scripts/Multiplayer/UINetworkStart.cs
@@ -0,0 +1,67 @@
+using Unity.Netcode;
+using UnityEngine;
+
+
+public class UINetworkStart : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_Text displayText;
+    [SerializeField] private TMPro.TMP_InputField lobbyCodeInput;
+
+    private void LobbyHelper(bool isPublic, string infoText)
+    {
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            Debug.Log("UINetworkStart - !NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer");
+            return;
+        }
+        
+        if (Multiplayer.Instance == null)  {
+            Debug.Log("UINetworkStart - Multiplayer.Instance == null");
+            return;
+        }
+        
+        Multiplayer.Instance.CreateLobby(isPublic);
+        displayText.SetText(infoText);
+    }
+    
+    public void CreatePublicLobby()
+    {
+        LobbyHelper(false, "Created Public Lobby");
+    }
+    
+    public void CreatePrivateLobby()
+    {
+        LobbyHelper(true, "Created Public Lobby");
+    }
+    
+    //TODO:FIXME Hookup to helper for multiplayer check
+    //TODO:FIXME Need some text feed back if you acctualy joined a lobby.
+    private void JoinPublicLobby()
+    {
+        Multiplayer.Instance.QuickJoinLobby();
+        //TODO:FIXME Need some validation check here for the text
+        displayText.SetText("Joined Random Lobby");
+    }
+    
+    
+    
+    //TODO:FIXME Hookup to helper for multiplayer check
+    
+    private void JoinPrivateLobby()
+    {
+        //"Join With Code"
+        //OnJoinWithCode(lobbyCodeInput);
+
+        string code = lobbyCodeInput.text;
+        
+        code = code.Trim().ToUpper();
+
+        if (string.IsNullOrEmpty(code))
+        {
+            Debug.LogWarning("No lobby code entered.");
+            return;
+        }
+
+        Multiplayer.Instance.JoinLobbyByCode(code);
+    }
+}
