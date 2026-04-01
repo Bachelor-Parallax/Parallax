dc7ac1d67f27c7bb10c1b5c572138a786cd3eee7
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-01 15:34:43 +0200

    Disabled magicCat for the time being

--- Parallax/Assets/💻 Scripts/MagicCat.cs
+++ Parallax/Assets/💻 Scripts/MagicCat.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class MagicCat : MonoBehaviour
 {
-    private void Start()
-    {
-        GameObject[] allObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
-
-        foreach (GameObject obj in allObjects)
-        {
-            if (obj.CompareTag("Player"))
-            {
-                Rigidbody rb = obj.GetComponent<Rigidbody>();
-                rb.isKinematic = false;
-            }
-        }
-    }
+    // TODO: Currently disabled, since rigidbody has been removed,
+    //  as it is not commonly used in conjuction with CharacterController.
+    
+    // private void Start()
+    // {
+    //     GameObject[] allObjects = FindObjectsByType<GameObject>(FindObjectsSortMode.None);
+    //
+    //     foreach (GameObject obj in allObjects)
+    //     {
+    //         if (obj.CompareTag("Player"))
+    //         {
+    //             Rigidbody rb = obj.GetComponent<Rigidbody>();
+    //             rb.isKinematic = false;
+    //         }
+    //     }
+    // }
 }
