7a0248a2deed4b4a811b896991fc7acfeeed00a8
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-02 11:18:23 +0200

    Update PerspectiveManager.cs
    Using network manager to find local player

--- Parallax/Assets/💻 Scripts/Asymmetry/PerspectiveManager.cs
+++ Parallax/Assets/💻 Scripts/Asymmetry/PerspectiveManager.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Linq;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PerspectiveManager : MonoBehaviour, IPerspectiveManager
 {
     private BaseAsymProperty[] _asymProperties;
 
-    private INetworkManager _networkManager;
+    private NetworkManager _networkManager;
 
     private void Awake()
     {
         _asymProperties = FindObjectsByType<BaseAsymProperty>(FindObjectsSortMode.None);
-        _networkManager = UnityExtensions.FindObjectsAssignableTo<INetworkManager>(FindObjectsSortMode.None)
-            .FirstOrDefault();
+        // _networkManager = UnityExtensions.FindObjectsAssignableTo<INetworkManager>(FindObjectsSortMode.None)
+        //     .FirstOrDefault();
+        _networkManager = FindFirstObjectByType<NetworkManager>();
 
         if (_networkManager == null)
         {
@@ -23,7 +25,15 @@ public class PerspectiveManager : MonoBehaviour, IPerspectiveManager
     public void ApplyPerspective()
     {
         // find target perspective
-        GameObject localPlayer = _networkManager.GetLocalPlayerGameObject();
+        //GameObject localPlayer = _networkManager.GetLocalPlayerGameObject();
+        if (_networkManager.LocalClient?.PlayerObject == null)
+        {
+            Debug.LogWarning("Local player not spawned yet.");
+            return;
+        }
+        
+        GameObject localPlayer = _networkManager.LocalClient.PlayerObject.gameObject;
+        
         PerspectiveProfile profile = localPlayer.tag switch
         {
             GameConstants.CAT_TAG => PerspectiveProfile.Cat,
