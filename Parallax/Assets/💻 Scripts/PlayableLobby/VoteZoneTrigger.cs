e019722a44ec71a2dd4fc2790b3dcb2f1914c0bf
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-14 21:07:13 +0200

    Update VoteZoneTrigger.cs

--- Parallax/Assets/💻 Scripts/PlayableLobby/VoteZoneTrigger.cs
+++ Parallax/Assets/💻 Scripts/PlayableLobby/VoteZoneTrigger.cs
@@ -1,4 +1,4 @@
-using System;
+using Unity.Netcode;
 using UnityEngine;
 
 public class VoteZoneTrigger : MonoBehaviour
@@ -12,14 +12,28 @@ public class VoteZoneTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player"))
+            return;
         
+        var netObj = other.GetComponent<NetworkObject>();
+
+        if (netObj == null || !netObj.IsOwner)
+            return;
+
+        Debug.Log($"Player {netObj.OwnerClientId} voted for {levelData.sceneName}");
+
         LobbyManager.Instance.VoteLevelServerRpc(levelData.sceneName);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player"))
+            return;
+        
+        var netObj = other.GetComponent<NetworkObject>();
+
+        if (netObj == null || !netObj.IsOwner)
+            return;
         
         LobbyManager.Instance.ToggleReady(false);
     }
