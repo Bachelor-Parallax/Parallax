c4aa7dcac205131c76b841052c9908ba83c114ca
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-14 14:47:55 +0200

    in game menu

---
+++ Parallax/Assets/💻 Scripts/Menu/InGameUI.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+    public void OnResumePressed()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnSettingsPressed()
+    {
+        
+    }
+
+    public void OnGuidePressed()
+    {
+        
+    }
+
+    public void OnRetryPressed()
+    {
+        MultiplayerManager.Instance.ReloadCurrentScene();
+    }
+
+    public void OnLeaveGamePressed()
+    {
+        _ = LeaveGame();
+    }
+
+    private async Task LeaveGame()
+    {
+        await MultiplayerManager.Instance.Disconnect();
+    }
+}
