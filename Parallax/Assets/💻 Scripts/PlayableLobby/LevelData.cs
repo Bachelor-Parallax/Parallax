44acdefb5d8f63a077e418352a77146c8feb9dee
Kasper Bruun Bentsen <202103249@post.au.dk>
2026-04-16 11:00:59 +0200

    ehm

--- Parallax/Assets/💻 Scripts/PlayableLobby/LevelData.cs
+++ Parallax/Assets/💻 Scripts/PlayableLobby/LevelData.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "LevelData", menuName = "Lobby/Level Data")]
 public class LevelData : ScriptableObject
 {
+    [Header("Preset data")]
     public string levelName;
     public string levelDescription;
-    public string bestTime;
     public string sceneName;
+    public string devTime;
+    public Material levelImage;
+    
+    [Header("Player data")]
+    public string bestTime;
+    public bool catTrophy;
+    public bool humanTrophy;
+    public bool beatDevTrophy;
 }
