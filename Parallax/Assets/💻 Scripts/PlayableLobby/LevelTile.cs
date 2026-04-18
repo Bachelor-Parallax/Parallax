3360119a15fe5aa742fed5e17b3c5a7a542ddfcc
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-18 09:38:23 +0200

    Restructure LevelTile

--- Parallax/Assets/💻 Scripts/PlayableLobby/LevelTile.cs
+++ Parallax/Assets/💻 Scripts/PlayableLobby/LevelTile.cs
@@ -8,7 +8,11 @@ public class LevelTile : MonoBehaviour
     [Header("UI")]
     public TextMeshPro levelNameText;
     public TextMeshPro bestTimeText;
+    public TextMeshPro devTimeText;
     public SpriteRenderer image;
+    public GameObject catTrophyObject;
+    public GameObject humanTrophyObject;
+    public GameObject devTimeTrophyObject;
 
     [Header("Voting")]
     public VoteZoneTrigger voteZone;
@@ -24,7 +28,12 @@ public class LevelTile : MonoBehaviour
 
         levelNameText.text = levelData.levelName;
         bestTimeText.text = levelData.bestTime;
+        devTimeText.text = levelData.devTime;
         image.sprite = levelData.levelImage;
+        
+        catTrophyObject.SetActive(levelData.catTrophy);
+        humanTrophyObject.SetActive(levelData.humanTrophy);
+        devTimeTrophyObject.SetActive(levelData.devTimeTrophy);
 
         voteZone.SetLevel(levelData);
     }
