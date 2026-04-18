5a470646194b74a001671c91f9ada05d0af6a8a1
FluffyAssassin <thea.lindgaard@gmail.com>
2026-04-18 15:57:58 +0200

    Update AsymGeometry.cs

--- Parallax/Assets/💻 Scripts/Asymmetry/AsymGeometry.cs
+++ Parallax/Assets/💻 Scripts/Asymmetry/AsymGeometry.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 
 public class AsymGeometry : BaseAsymProperty<MeshFilter>
 {
+    [InfoBox("Attached Mesh Filter component required!\nMesh updated at runtime to make object appear visually different")]
     [SerializeField] private Mesh _humanMesh;
     [SerializeField] private Mesh _catMesh;
 
-    public override void ApplyPerspectiveProfile(PerspectiveProfile profile)
+    public override void ApplyPerspectiveProfile(CharacterRole role)
     {
-        AsymComponent.mesh = profile switch
+        AsymComponent.mesh = role switch
         {
-            PerspectiveProfile.Human => _humanMesh,
-            PerspectiveProfile.Cat => _catMesh,
-            _ => throw new ArgumentException("Unexpected perspective profile")
+            CharacterRole.Human => _humanMesh,
+            CharacterRole.Cat => _catMesh,
+            _ => throw new ArgumentException("Unexpected CharacterRole")
         };
     }
 }
