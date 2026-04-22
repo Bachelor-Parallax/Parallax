e48fe9a0c09b4cfe0ec54aa0325eb77b494bc754
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-22 09:30:11 +0200

    Set max players to 1

--- Parallax/Assets/💻 Scripts/GameConstants.cs
+++ Parallax/Assets/💻 Scripts/GameConstants.cs
@@ -2,5 +2,5 @@ public static class GameConstants
 {
     public const string PLAYER_TAG = "Player";
     public const string LOBBY_SCENE_NAME = "PlayableLobby";
-    public const int MAX_PLAYERS = 2;
+    public const int MAX_PLAYERS = 1;
 }
