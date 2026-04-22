84c2896d85909e773dfc2c3fb0d4cda48d5307d2
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-04-22 09:24:48 +0200

    Downloaded spinners and added vote text to LevelTile prefab

--- Parallax/Assets/💻 Scripts/PlayableLobby/VoteZoneTrigger.cs
+++ Parallax/Assets/💻 Scripts/PlayableLobby/VoteZoneTrigger.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
 public class VoteZoneTrigger : NetworkBehaviour
 {
-    public LevelData levelData;
+    [SerializeField] private TMP_Text voteText;
+    [SerializeField] private LevelData levelData;
 
     private HashSet<ulong> voters = new HashSet<ulong>();
 
@@ -104,41 +106,4 @@ public class VoteZoneTrigger : NetworkBehaviour
 
         countdownCoroutine = null;
     }
-    
-    // public LevelData levelData;
-    //
-    // public void SetLevel(LevelData data)
-    // {
-    //     levelData = data;
-    // }
-    //
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     Debug.Log("OnTriggerEnter fired");
-    //     if (!other.CompareTag("Player"))
-    //         return;
-    //     
-    //     var netObj = other.GetComponent<NetworkObject>();
-    //
-    //     if (netObj == null || !netObj.IsOwner)
-    //         return;
-    //
-    //     Debug.Log($"Player {netObj.OwnerClientId} voted for {levelData.sceneName}");
-    //
-    //     LobbyManager.Instance.VoteLevelServerRpc(levelData.sceneName);
-    // }
-    //
-    // private void OnTriggerExit(Collider other)
-    // {
-    //     Debug.Log("OnTriggerExit fired");
-    //     if (!other.CompareTag("Player"))
-    //         return;
-    //     
-    //     var netObj = other.GetComponent<NetworkObject>();
-    //
-    //     if (netObj == null || !netObj.IsOwner)
-    //         return;
-    //     
-    //     LobbyManager.Instance.ToggleReady(false);
-    // }
 }
