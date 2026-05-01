975eb6fcffbf3824c05b6a143a68a712856c3157
amarstang <35458474+amarstang@users.noreply.github.com>
2026-05-01 11:21:29 +0200

    menus working

--- Parallax/Assets/💻 Scripts/Menu/Pause.cs
+++ Parallax/Assets/💻 Scripts/Menu/Pause.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Pause : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenuContainer;
     [SerializeField] private GameObject pauseMenu;
-
+    [SerializeField] private List<GameObject> gameObjects;
+    
     private InputAction pauseAction;
 
     private void Awake()
@@ -21,14 +24,42 @@ public class Pause : MonoBehaviour
 
     private void OnPause(InputAction.CallbackContext context)
     {
-        bool isActive = !pauseMenu.activeSelf;
-        pauseMenu.SetActive(isActive);
+        bool isActive = !pauseMenuContainer.activeSelf;
+        pauseMenuContainer.SetActive(isActive);
         Cursor.visible = isActive;
+
+        if (isActive) // Makes sure that the PauseMenu is the first menu showen each time
+        {
+            foreach (GameObject obj in gameObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+        else pauseMenu.gameObject.SetActive(true);
     }
 
     public void Resume()
     {
         Cursor.visible = false;
-        pauseMenu.SetActive(false);
+        pauseMenuContainer.SetActive(false);
+        
+        foreach (GameObject obj in gameObjects)
+        {
+            obj.SetActive(false);
+        }
     }
+
+    public void Disconnect()
+    {
+        if (MultiplayerManager.Instance == null) return;
+        
+        _ = MultiplayerManager.Instance.Disconnect();
+    }
+
+    public void Retry()
+    {
+        if (SceneLoader.Instance == null) return;
+        SceneLoader.Instance.ReloadCurrentScene();
+    }
+    
 }
