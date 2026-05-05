8e547ab6397b760855ae0180a652ff324bf2f121
amarstang <35458474+amarstang@users.noreply.github.com>
2026-05-05 23:13:51 +0200

    1. Hooked up CharacterSound with sound slider events 2. fixed LobbyMenuCanvas setting page close button

--- Parallax/Assets/💻 Scripts/CharacterController/CharacterSound.cs
+++ Parallax/Assets/💻 Scripts/CharacterController/CharacterSound.cs
@@ -1,61 +1,121 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(AudioSource))]
 public class CharacterSound : NetworkBehaviour
 {
-    [SerializeField] private AudioClip[] catSounds;
-    [SerializeField] private AudioClip[] humanSounds;
-    [SerializeField] private InputActionReference soundAction;
+	[SerializeField] private AudioClip[] catSounds;
+	[SerializeField] private AudioClip[] humanSounds;
+	[SerializeField] private InputActionReference soundAction;
 
-    private AudioSource audioSource;
+	private AudioSource audioSource;
+	private SettingsManager _settingsManager;
+	
+	private void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+	private void OnEnable()
+	{
+		if (soundAction == null) return;
+		
+		soundAction.action.performed += OnSound;
+		soundAction.action.Enable();
+		
+		// SceneManager.sceneLoaded += OnSceneLoaded;
+		//
+		// // Subscribe
+		// _settingsManager.OnMusicVolumeChanged += UpdateDialogueVolume;
+		// _settingsManager.OnMuteChanged += UpdateMute;
+	}
 
-    private void OnEnable()
-    {
-        if (soundAction == null) return;
+	private void OnDisable()
+	{
+		if (soundAction == null) return;
 
-        soundAction.action.performed += OnSound;
-        soundAction.action.Enable();
-    }
+		soundAction.action.performed -= OnSound;
+		soundAction.action.Disable();
+		
+		// SceneManager.sceneLoaded -= OnSceneLoaded;
+		//
+		// // Unsubscribe
+		// _settingsManager.OnMusicVolumeChanged -= UpdateDialogueVolume;
+		// _settingsManager.OnMuteChanged -= UpdateMute;
+		
+	}
+	
+	private void OnSound(InputAction.CallbackContext context)
+	{
+		if (!IsLocalPlayer) return;
 
-    private void OnDisable()
-    {
-        if (soundAction == null) return;
+		PlaySoundServerRpc();
+	}
 
-        soundAction.action.performed -= OnSound;
-        soundAction.action.Disable();
-    }
-    
-    private void OnSound(InputAction.CallbackContext context)
-    {
-        if (!IsLocalPlayer) return;
+	[ServerRpc]
+	private void PlaySoundServerRpc()
+	{
+		PlaySoundClientRpc();
+	}
 
-        PlaySoundServerRpc();
-    }
+	[ClientRpc]
+	private void PlaySoundClientRpc()
+	{
+		switch (GetComponent<RoleController>().role.Value)
+		{
+			case CharacterRole.Human:
+				audioSource.PlayOneShot(humanSounds[Random.Range(0, humanSounds.Length)]);
+				break;
+			case CharacterRole.Cat:
+				audioSource.PlayOneShot(catSounds[Random.Range(0, catSounds.Length)]);
+				break;
+		}
+	}
 
-    [ServerRpc]
-    private void PlaySoundServerRpc()
-    {
-        PlaySoundClientRpc();
-    }
 
-    [ClientRpc]
-    private void PlaySoundClientRpc()
-    {
-        switch (GetComponent<RoleController>().role.Value)
-        {
-            case CharacterRole.Human:
-                audioSource.PlayOneShot(humanSounds[Random.Range(0, humanSounds.Length)]);
-                break;
-            case CharacterRole.Cat:
-                audioSource.PlayOneShot(catSounds[Random.Range(0, catSounds.Length)]);
-                break;
-        }
-    }
+	public void SoundSetup() // The setup on now sceen load
+	{
+		// Gets the instance
+		_settingsManager = SettingsManager.Instance;
+
+		// Subscribes
+		_settingsManager.OnMuteChanged += UpdateMute;
+		_settingsManager.OnDialogueVolumeChanged += UpdateDialogueVolume;
+		
+		// Fetches values from PlayerPrefs sync them (should only run ONCE)
+		UpdateDialogueVolume(_settingsManager.GetDialogueVolume());
+		UpdateMute(_settingsManager.GetMute());
+	}
+
+
+	// Event handling
+	void UpdateDialogueVolume(float value) // 'value' is the value from UpdateDialogueVolume event call
+	{
+		// Debug.LogWarning("UpdateMusic value = " + value);
+		// Debug.LogWarning("Master volume = " + SettingsManager.Instance.GetMasterVolume());
+		double volume = Math.Round(value * _settingsManager.GetMasterVolume(), 2);
+		// Debug.LogWarning("Volume = " + volume);
+		audioSource.volume = (float)volume;
+	}
+	
+	void UpdateMute(bool value)
+	{
+		// Debug.LogWarning("UpdateMute" + audioSource.mute + value);
+		audioSource.mute = value;
+	}
+	
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		// Unsubscribe from any old event
+		_settingsManager.OnMusicVolumeChanged -= UpdateDialogueVolume;
+		_settingsManager.OnMuteChanged -= UpdateMute;
+		
+		SoundSetup();
+	}
 }
--- Parallax/Assets/💻 Scripts/Menu/PlayerPrefs/SettingsManager.cs
+++ Parallax/Assets/💻 Scripts/Menu/PlayerPrefs/SettingsManager.cs
@@ -5,150 +5,149 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public static SettingsManager Instance;
+	public static SettingsManager Instance;
 
-    // EVENTS
-    public event Action<float> OnMasterVolumeChanged;
-    public event Action<bool> OnMuteChanged;
-    public event Action<float> OnCamaraSensitivityChanged;
-    public event Action<bool> OnConfineMouseChanged;
-    public event Action<float> OnSFXVolumeChanged;
-    public event Action<float> OnDialogueVolumeChanged;
-    public event Action<float> OnMusicVolumeChanged;
-    
+	// EVENTS
+	public event Action<float> OnMasterVolumeChanged;
+	public event Action<bool> OnMuteChanged;
+	public event Action<float> OnCamaraSensitivityChanged;
+	public event Action<bool> OnConfineMouseChanged;
+	public event Action<float> OnSFXVolumeChanged;
+	public event Action<float> OnDialogueVolumeChanged;
+	public event Action<float> OnMusicVolumeChanged;
+	
 
-    // PlayerPrefs keys
-    private const string MasterVolumeKey = "master_volume";
-    private const string MusicVolumeKey = "music_volume";
-    private const string SFXVolumeKey = "sfx_volume";
-    private const string DialogueVolumeKey = "dialogue_volume";
+	// PlayerPrefs keys
+	private const string MasterVolumeKey = "master_volume";
+	private const string MusicVolumeKey = "music_volume";
+	private const string SFXVolumeKey = "sfx_volume";
+	private const string DialogueVolumeKey = "dialogue_volume";
 
-    private const string MuteKey = "mute_all";
-    private const string SensitivityKey = "camara_sensitivity";
-    private const string ConfineMouseKey = "confine_mouse";
+	private const string MuteKey = "mute_all";
+	private const string SensitivityKey = "camara_sensitivity";
+	private const string ConfineMouseKey = "confine_mouse";
 
-    private void Awake()
-    {
-        Instance = this;
-        LoadAll();
-    }
+	private void Awake()
+	{
+		Instance = this;
+		LoadAll();
+	}
 
+	// SETTERS
+	public void SetMasterVolume(Slider sliderValue)
+	{
+		PlayerPrefs.SetFloat(MasterVolumeKey, sliderValue.value);
+		PlayerPrefs.Save();
+		ApplyVolume();
+	}
 
-    // SETTERS
-    public void SetMasterVolume(Slider sliderValue)
-    {
-        PlayerPrefs.SetFloat(MasterVolumeKey, sliderValue.value);
-        PlayerPrefs.Save();
-        ApplyVolume();
-    }
+	public void ToggleMute()
+	{
+		bool muted = !GetMute();
 
-    public void ToggleMute()
-    {
-        bool muted = !GetMute();
+		PlayerPrefs.SetInt(MuteKey, muted ? 1 : 0);
+		PlayerPrefs.Save();
 
-        PlayerPrefs.SetInt(MuteKey, muted ? 1 : 0);
-        PlayerPrefs.Save();
+		ApplyVolume();
+	}
+	
+	public void SetMusicVolume(Slider sliderValue)
+	{ 
+		PlayerPrefs.SetFloat(MusicVolumeKey, sliderValue.value);
+		PlayerPrefs.Save();
+		ApplyVolume();
+	}
 
-        ApplyVolume();
-    }
-    
-    public void SetMusicVolume(Slider sliderValue)
-    { 
-        PlayerPrefs.SetFloat(MusicVolumeKey, sliderValue.value);
-        PlayerPrefs.Save();
-        ApplyVolume();
-    }
+	public void SetSFXVolume(Slider sliderValue)
+	{
+		PlayerPrefs.SetFloat(SFXVolumeKey, sliderValue.value);
+		PlayerPrefs.Save();
+		ApplyVolume();
+	}
 
-    public void SetSFXVolume(Slider sliderValue)
-    {
-        PlayerPrefs.SetFloat(SFXVolumeKey, sliderValue.value);
-        PlayerPrefs.Save();
-        ApplyVolume();
-    }
+	public void SetDialogueVolume(Slider sliderValue)
+	{
+		PlayerPrefs.SetFloat(DialogueVolumeKey, sliderValue.value);
+		PlayerPrefs.Save();
+		ApplyVolume();
+	}
 
-    public void SetDialogueVolume(Slider sliderValue)
-    {
-        PlayerPrefs.SetFloat(DialogueVolumeKey, sliderValue.value);
-        PlayerPrefs.Save();
-        ApplyVolume();
-    }
+	public void SetMouseSensitivity(Slider sliderValue)
+	{
+		PlayerPrefs.SetFloat(SensitivityKey, sliderValue.value);
+		PlayerPrefs.Save();
 
-    public void SetMouseSensitivity(Slider sliderValue)
-    {
-        PlayerPrefs.SetFloat(SensitivityKey, sliderValue.value);
-        PlayerPrefs.Save();
+		OnCamaraSensitivityChanged?.Invoke(sliderValue.value);
+	}
 
-        OnCamaraSensitivityChanged?.Invoke(sliderValue.value);
-    }
+	public void ToggleConfineMouse()
+	{
+		bool value = !GetConfineMouse();
 
-    public void ToggleConfineMouse()
-    {
-        bool value = !GetConfineMouse();
+		PlayerPrefs.SetInt(ConfineMouseKey, value ? 1 : 0);
+		PlayerPrefs.Save();
 
-        PlayerPrefs.SetInt(ConfineMouseKey, value ? 1 : 0);
-        PlayerPrefs.Save();
+		ApplyCursor();
+	}
 
-        ApplyCursor();
-    }
+	// -------------------------
+	// APPLY METHODS
+	// ------------------------
+	
+	private void ApplyVolume()
+	{
+		bool muted = GetMute();
+		OnMasterVolumeChanged?.Invoke(GetMasterVolume());
+		OnMuteChanged?.Invoke(GetMute());
+		
+		OnMusicVolumeChanged?.Invoke(GetMusicVolume());
+		OnSFXVolumeChanged?.Invoke(GetSFXVolume());
+		OnDialogueVolumeChanged?.Invoke(GetDialogueVolume());
+	}
+	
+	
+	private void ApplyCursor()
+	{
+		bool confine = GetConfineMouse();
+		
+		Cursor.lockState = confine ? CursorLockMode.Confined : CursorLockMode.None;
+		
+		OnConfineMouseChanged?.Invoke(confine);
+	}
 
-    // -------------------------
-    // APPLY METHODS
-    // ------------------------
-    
-    private void ApplyVolume()
-    {
-        bool muted = GetMute();
-        OnMasterVolumeChanged?.Invoke(GetMasterVolume());
-        OnMuteChanged?.Invoke(GetMute());
-        
-        OnMusicVolumeChanged?.Invoke(GetMusicVolume());
-        OnSFXVolumeChanged?.Invoke(GetSFXVolume());
-        OnDialogueVolumeChanged?.Invoke(GetDialogueVolume());
-    }
-    
-    
-    private void ApplyCursor()
-    {
-        bool confine = GetConfineMouse();
-        
-        Cursor.lockState = confine ? CursorLockMode.Confined : CursorLockMode.None;
-        
-        OnConfineMouseChanged?.Invoke(confine);
-    }
+	// -------------------------
+	// LOAD
+	// -------------------------
+	private void LoadAll()
+	{
+		ApplyVolume();
+		ApplyCursor();
 
-    // -------------------------
-    // LOAD
-    // -------------------------
-    private void LoadAll()
-    {
-        ApplyVolume();
-        ApplyCursor();
+		float sensitivity = GetMouseSensitivity();
+		OnCamaraSensitivityChanged?.Invoke(sensitivity);
+	}
 
-        float sensitivity = GetMouseSensitivity();
-        OnCamaraSensitivityChanged?.Invoke(sensitivity);
-    }
+	// -------------------------
+	// GETTERS
+	// -------------------------
+	public float GetMasterVolume() =>
+		PlayerPrefs.GetFloat(MasterVolumeKey, 1f);
 
-    // -------------------------
-    // GETTERS
-    // -------------------------
-    public float GetMasterVolume() =>
-        PlayerPrefs.GetFloat(MasterVolumeKey, 1f);
+	public bool GetMute() =>
+		PlayerPrefs.GetInt(MuteKey, 0) == 1;
+	
+	public float GetMusicVolume() =>
+		PlayerPrefs.GetFloat(MusicVolumeKey, 1f);
 
-    public bool GetMute() =>
-        PlayerPrefs.GetInt(MuteKey, 0) == 1;
-    
-    public float GetMusicVolume() =>
-        PlayerPrefs.GetFloat(MusicVolumeKey, 1f);
+	public float GetSFXVolume() =>
+		PlayerPrefs.GetFloat(SFXVolumeKey, 1f);
 
-    public float GetSFXVolume() =>
-        PlayerPrefs.GetFloat(SFXVolumeKey, 1f);
+	public float GetDialogueVolume() =>
+		PlayerPrefs.GetFloat(DialogueVolumeKey, 1f);
 
-    public float GetDialogueVolume() =>
-        PlayerPrefs.GetFloat(DialogueVolumeKey, 1f);
-
-    public float GetMouseSensitivity() =>
-        PlayerPrefs.GetFloat(SensitivityKey, 1f);
-    
-    public bool GetConfineMouse() =>
-        PlayerPrefs.GetInt(ConfineMouseKey, 0) == 1;
+	public float GetMouseSensitivity() =>
+		PlayerPrefs.GetFloat(SensitivityKey, 1f);
+	
+	public bool GetConfineMouse() =>
+		PlayerPrefs.GetInt(ConfineMouseKey, 0) == 1;
 }
