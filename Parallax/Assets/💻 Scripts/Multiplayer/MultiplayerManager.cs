8221ee99dd3d04a58e709e062ce18e0d796e4faf
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2026-05-14 23:07:47 +0200

    Update MultiplayerManager.cs

--- Parallax/Assets/💻 Scripts/Multiplayer/MultiplayerManager.cs
+++ Parallax/Assets/💻 Scripts/Multiplayer/MultiplayerManager.cs
@@ -119,7 +119,7 @@ public class MultiplayerManager : PersistentSingleton<MultiplayerManager>
     {
         if (currentLobby == null) return;
         
-        if (NetworkManager.Singleton.LocalClientId == clientId)
+        if (!NetworkManager.Singleton.IsHost && NetworkManager.Singleton.LocalClientId == clientId)
             SceneManager.LoadScene("MainMenu");
         
         if (!NetworkManager.Singleton.IsHost) return;
