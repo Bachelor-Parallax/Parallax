46216d464bc3b4000a46f7d7d2408d56bc1cb23d
amarstang <35458474+amarstang@users.noreply.github.com>
2026-05-23 23:16:04 +0200

    GameTimer test 1

---
+++ Parallax/Assets/💻 Scripts/GameTimer.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameTimer : NetworkBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+
+    private NetworkVariable<double> sceneStartTime = new();
+
+    private void Start()
+    {
+        if (IsServer)
+        {
+            sceneStartTime.Value =
+                NetworkManager.ServerTime.Time;
+        }
+
+        InvokeRepeating(nameof(UpdateTimerText), 0f, 0.1f);
+    }
+
+    private void UpdateTimerText()
+    {
+        double elapsed =
+            NetworkManager.ServerTime.Time
+            - sceneStartTime.Value;
+
+        int hours = Mathf.FloorToInt((float)(elapsed / 3600));
+        int minutes = Mathf.FloorToInt((float)(elapsed % 3600 / 60));
+        int seconds = Mathf.FloorToInt((float)(elapsed % 60));
+        int miliSeconds = Mathf.FloorToInt((float)(elapsed * 10f)) % 10;
+
+        timerText.SetText(
+            "{0:00}:{1:00}:{2:00}.{3}",
+            hours,
+            minutes,
+            seconds,
+            miliSeconds
+        );
+    }
+}
